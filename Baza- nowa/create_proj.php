708fe8faaf7549bd8a37a6f09aa1c3d53324ca07
Kuliniak <adkuli95@gmail.com>
2017-05-30 22:04:59 +0200

    Update create_proj.php
    powrót do starej wersji

--- Baza- nowa/create_proj.php
+++ Baza- nowa/create_proj.php
@@ -1,18 +1,18 @@
 <!-- Strona na której tworzy się projekty -->
 <?php
-require('Szablon/header.php');
+require('header.php');
 require('connect.php');
 ?>
 
 <?php
-if(isset($_SESSION['zalogowany']) && $_SESSION['zalogowany']==true) {
+if(isset($_SESSION['zalogowany']) && $_SESSION['zalogowany']==true && isset($_SESSION['admin']) && $_SESSION['admin'] == 1) {
 	?>
 
 	<h1>Stwórz nowy projekt</h1>
 
 	<div id="form_div">
 	<form method="post" action="">
-		<table id="tablica" style="width:100%">
+		<table id="tablica">
 			Nazwa projektu
 			<input type="text" name="nazwa" placeholder="nazwa projektu"><br>
 			Początek projektu:
@@ -42,7 +42,6 @@ if(isset($_SESSION['zalogowany']) && $_SESSION['zalogowany']==true) {
 	<?php
 	if(isset($_POST['wyslij'])) {
 		$ok = true;
-
 		$nazwa = $_POST['nazwa'];
 		$start_proj = $_POST['start'];
 		$koniec_proj = $_POST['koniec'];
@@ -51,9 +50,11 @@ if(isset($_SESSION['zalogowany']) && $_SESSION['zalogowany']==true) {
 		$do = $_POST['do'];
 		$rola = $_POST['rola'];
 		$koment = $_POST['komentarz'];
-
 	  	$q_znajdz_zad = "SELECT * FROM projekty WHERE nazwa='$nazwa' LIMIT 1";
-
+	  	if($nazwa==NULL) {
+	  		$ok = false;
+	    	echo '<br><font color="red">Nazwa projektu jest pusta!</font><br>';
+	  	}
 	  	if($result = mysqli_query($conn, $q_znajdz_zad)) {
 	    	  $row_cnt = mysqli_num_rows($result);
 	    	  if($row_cnt > 0) {
@@ -61,25 +62,32 @@ if(isset($_SESSION['zalogowany']) && $_SESSION['zalogowany']==true) {
 	    	    echo '<br><font color="red">Nazwa projektu jest już zajęta!</font><br>';
 	    	  }
 	  	}
-
+	  	if($start_proj==NULL || $koniec_proj==NULL) {
+	  		$ok = false;
+	      	echo '<font color="red">Początek i koniec projektu nie może być pusty</font>';
+	  	}
+	  	$ile = count($zadanie);
+		for($i = 0; $i < $ile; $i++){
+			if($zadanie[$i]==NULL || $od==NULL || $do==NULL) {
+				$ok = false;
+				echo '<br><font color="red">Nie zostawiaj pustych wierszy</font><br>';
+				break;
+			}
+		}
 	  	if($ok == true) {
 	  		$q_znajdz_id = "SELECT MAX(id_projektu) FROM projekty";
-
 			if($result = mysqli_query($conn, $q_znajdz_id)) {
 				$row = mysqli_fetch_assoc($result);
 				$id_projektu = $row['MAX(id_projektu)']+1;
 			}
-
 			//dodanie projektu------------------------------------------------------
 		    $dodaj_proj = "INSERT INTO projekty (id_projektu, nazwa, start_proj, koniec_proj, komentarze) VALUES ('$id_projektu', '$nazwa', '$start_proj', '$koniec_proj', '$koment')";
-
 		    if (mysqli_query($conn, $dodaj_proj)) {
 		      	echo "Dodano nowy projekt!";
 		    }else{
 		      	echo "Error: " . $dodaj_proj . "<br>" . mysqli_error($conn);
 		    }
 			//-----------------------------------------------------------------------
-
 			//dodawanie zadań--------------------------------------------------------
 	    	$ile = count($zadanie);
 		    for($i = 0; $i < $ile; $i++){
@@ -91,10 +99,8 @@ if(isset($_SESSION['zalogowany']) && $_SESSION['zalogowany']==true) {
 		    	}
 		    }
 			//------------------------------------------------------------------------
-
 			//dodanie dostepu---------------------------------------------------------
-		    $q_dodaj_dostep = "INSERT INTO dostepy (id_dostepu, id_osoby, id_projektu, rola, admin) VALUES (NULL, '".$_SESSION['id_osoby']."', '$id_projektu', '$rola', '".$_SESSION['id_osoby']."')";
-
+		    $q_dodaj_dostep = "INSERT INTO dostepy (id_dostepu, id_osoby, id_projektu, rola, admin) VALUES (NULL, '".$_SESSION['id_osoby']."', '$id_projektu', '$rola', '')";
 			if(mysqli_query($conn, $q_dodaj_dostep)) {
 				echo "<br>Masz dostęp do tego projektu";
 		    }else{
@@ -107,15 +113,14 @@ if(isset($_SESSION['zalogowany']) && $_SESSION['zalogowany']==true) {
 
 	<?php
 } else {
-	if(isset($_SESSION['zalogowany']) && $_SESSION['zalogowany']==true){
-		header("Location: zalogowany.php");
-	} else {
-		header("Location: index.php");
-	}
+		if(isset($_SESSION['zalogowany']) && $_SESSION['zalogowany']==true){
+			header("Location: zalogowany.php");
+		} else {
+			header("Location: index.php");
+		}
 }
-
 ?>
 
 <?php
-require('Szablon/footer.php');
+require('footer.php');
 ?>
